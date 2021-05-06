100a917aec8e7206a9072480080b50ac3648bcb2
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-05-06 18:03:58 -0300

    Criado o projeto

---
+++ src/main/java/br/com/karatedopi/KaratedopiApplication.java
@@ -0,0 +1,13 @@
+package br.com.karatedopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaratedopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaratedopiApplication.class, args);
+	}
+
+}
