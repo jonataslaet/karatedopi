1c8596dc7578fa8f1f120a4ae7b9bb12ca643a92
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-05-09 17:54:59 -0300

    Implementada paginação no endpoint GET /municipios

--- src/main/java/br/com/karatedopi/controllers/MunicipioController.java
+++ src/main/java/br/com/karatedopi/controllers/MunicipioController.java
@@ -1,15 +1,17 @@
 package br.com.karatedopi.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.karatedopi.controllers.dtos.MunicipioDTO;
+import br.com.karatedopi.domain.enums.EstadoEnum;
 import br.com.karatedopi.services.EstadoService;
 import br.com.karatedopi.services.MunicipioService;
 
@@ -24,7 +26,9 @@ public class MunicipioController {
 	private MunicipioService municipioService;
 	
 	@GetMapping
-	public ResponseEntity<List<MunicipioDTO>> listaMunicipios(){
-		return municipioService.retornaMunicipios();
+	public Page<MunicipioDTO> listaMunicipios(@RequestParam(required=false) EstadoEnum uf, @PageableDefault(page = 0, size=10, sort="id", direction = Direction.DESC) Pageable paginacao){
+		return municipioService.retornaMunicipios(uf, paginacao);
 	}
+	
+	
 }
--- src/main/java/br/com/karatedopi/services/MunicipioService.java
+++ src/main/java/br/com/karatedopi/services/MunicipioService.java
@@ -1,22 +1,28 @@
 package br.com.karatedopi.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.karatedopi.controllers.dtos.MunicipioDTO;
+import br.com.karatedopi.domain.Municipio;
+import br.com.karatedopi.domain.enums.EstadoEnum;
 import br.com.karatedopi.repositories.MunicipioRepository;
 
 @Service
 public class MunicipioService {
-	@Autowired
-	MunicipioRepository municipioRepository;
 	
-	public ResponseEntity<List<MunicipioDTO>> retornaMunicipios(){
-		List<MunicipioDTO> municipiosEncontrados = MunicipioDTO.municipiosDto(municipioRepository.findAll());
-		return ResponseEntity.ok().body(municipiosEncontrados);
+	@Autowired
+	private MunicipioRepository municipioRepository;
+	
+	public Page<MunicipioDTO> retornaMunicipios(EstadoEnum uf, Pageable paginacao){
+		if (uf == null) {
+			Page<Municipio> municipios = municipioRepository.findAll(paginacao);
+			return municipios.map(MunicipioDTO::new);
+		}
+		Page<Municipio> municipios = municipioRepository.findAllByEstadoUf(uf, paginacao);
+		return municipios.map(MunicipioDTO::new);
 	}
 
 }
