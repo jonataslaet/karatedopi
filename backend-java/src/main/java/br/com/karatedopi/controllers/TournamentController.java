3c4e4b418f104cd37e086fb484675ee224db2674
jonataslaet <jonataslaetprogramador@gmail.com>
2023-10-13 11:05:10 -0300

    0008: Tournament deleting and getting endpoints

--- backend-java/src/main/java/br/com/karatedopi/controllers/TournamentController.java
+++ backend-java/src/main/java/br/com/karatedopi/controllers/TournamentController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -43,5 +45,17 @@ public class TournamentController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTournament.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(createdTournament);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteTournament(@PathVariable("id") Long id){
+		tournamentService.deleteTournamentById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<TournamentDTO> getTournament(@PathVariable("id") Long id){
+		TournamentDTO gotTournament = tournamentService.getTournamentById(id);
+		return ResponseEntity.ok().body(gotTournament);
 	}
 }
--- backend-java/src/main/java/br/com/karatedopi/services/TournamentService.java
+++ backend-java/src/main/java/br/com/karatedopi/services/TournamentService.java
@@ -6,6 +6,7 @@ import br.com.karatedopi.entities.City;
 import br.com.karatedopi.entities.Tournament;
 import br.com.karatedopi.repositories.AddressRepository;
 import br.com.karatedopi.repositories.TournamentRepository;
+import br.com.karatedopi.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -62,6 +63,22 @@ public class TournamentService {
 
 	private City getCity(TournamentDTO tournamentDTO) {
 		return cityService.getCityByCityNameAndStateName(tournamentDTO.getAddress().getCity(), tournamentDTO.getAddress().getState());
+	}
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+    public void deleteTournamentById(Long id) {
+		findTournamentById(id);
+		tournamentRepository.deleteById(id);
+	}
+
+	public TournamentDTO getTournamentById(Long id) {
+		Tournament tournament = findTournamentById(id);
+		return TournamentDTO.getTournamentDTO(tournament);
+	}
+
+	private Tournament findTournamentById(Long id) {
+		return tournamentRepository.findById(id).orElseThrow(() ->
+			new ResourceNotFoundException("No tournament with id " + id + " was found"));
 	}
 }
 
