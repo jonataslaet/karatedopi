30e6b4396e39826836c3dea372621a43bcfa6609
jonataslaet <jonataslaetprogramador@gmail.com>
2023-12-20 09:43:41 -0300

    0014: Adjustments for registration, roles and stateService
    Added two gets for UserDetailsProjection: firstname and lastname
    Fixed validUserExistence and put transactional on delete method
    Included method to removing all roles whose authority is bigger than in the evaluation role

--- backend-java/src/main/java/br/com/karatedopi/entities/UserDetailsProjection.java
+++ backend-java/src/main/java/br/com/karatedopi/entities/UserDetailsProjection.java
@@ -3,6 +3,8 @@ package br.com.karatedopi.entities;
 public interface UserDetailsProjection {
 
     Long getId();
+    String getFirstname();
+    String getLastname();
     String getUsername();
     String getPassword();
     Long getRoleId();
