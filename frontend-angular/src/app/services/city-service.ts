c1ea02b3757dace0fbd0868b4d9c687203f4568e
jonataslaet <jonataslaetprogramador@gmail.com>
2024-02-13 22:05:30 -0300

    0032: Configured environments to front-end

--- frontend-angular/src/app/services/city-service.ts
+++ frontend-angular/src/app/services/city-service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { City } from "../common/city";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
-    private statesUrl = 'http://localhost:8080/cities/all';
+    
+    private statesUrl = `${environment.apiUrl}/cities/all`;
   
     constructor(private httpClient: HttpClient) { }
 
--- frontend-angular/src/app/services/request.service.ts
+++ frontend-angular/src/app/services/request.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class RequestService {
-    private baseUrl = 'http://localhost:8080';
+    private baseUrl = `${environment.apiUrl}`;
 
     constructor(private httpClient: HttpClient) {}
 
